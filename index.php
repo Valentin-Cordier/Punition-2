ec5b8060e9f684c37f6df98560c407bcd26db2bd
Valentin-Cordier <v.cordier@simplon-charleville.fr>
2019-04-23 15:27:10 +0200

    début Punition-2

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+      <head>
+        <meta charset="utf-8" />
+            <title>Punition-2</title>
+      </head>
+      <body>
+        <?php
+             $A = "98";
+             $B = "97";
+             $C = "95";
+
+
+             if ($A < $B == $C) {
+               // code...
+               echo "$A";
+             } else {
+               // code...
+               echo "$B";
+             } else {
+               // code...
+               echo "$C";
+             };
+
+
+
+
+        ?>
