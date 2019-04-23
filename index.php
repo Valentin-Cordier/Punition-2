7a31c583f366e78153a718a2cce1cde653b8c076
Valentin-Cordier <v.cordier@simplon-charleville.fr>
2019-04-23 15:46:04 +0200

    Question 3 faite  ou presque

--- index.php
+++ index.php
@@ -9,13 +9,23 @@
              $A = "98";
              $B = "97";
              $C = "96";
+             $nbr1 = "nbr1";
+             $nbr2 = "nbr2";
 
             echo "Question 1 et 2: ";
             echo "<br>";
             echo min($A, $B, $C);
+            echo "<br>";
 
             echo "Question 3:";
             echo "<br>";
+            if ($nbr1 >= 1 AND $nbr2 <= 100) {
+              // code...
+              echo "Les valeurs sont comprises entre 1 et 100";
+            } else {
+              // code...
+              echo "Les valeurs ne sont pas comprises entre 1 et 100";
+            }
 
 
 
