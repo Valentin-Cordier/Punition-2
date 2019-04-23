b9669f364b3d2ba91fc687af840c1cfe8213a5e4
Valentin-Cordier <v.cordier@simplon-charleville.fr>
2019-04-23 15:51:16 +0200

    Question 4

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
              $A = "98";
              $B = "97";
              $C = "96";
-             $nbr1 = "nbr1";
-             $nbr2 = "nbr2";
+             $nbr1 = "1";
+             $nbr2 = "100";
 
             echo "Question 1 et 2: ";
             echo "<br>";
@@ -25,6 +25,17 @@
             } else {
               // code...
               echo "Les valeurs ne sont pas comprises entre 1 et 100";
+            }
+
+            echo "<br>";
+            echo "Question 4:";
+            echo "<br>";
+            if ($nbr1 >= 5 AND $nbr2 <= 100) {
+              // code...
+              echo "Les valeurs sont comprises entre 5 et 100";
+            } else {
+              // code...
+              echo "Les valeurs ne sont pas comprises entre 5 et 100";
             }
 
 
